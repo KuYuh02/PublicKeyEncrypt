e6f3979dafdd2d173b365b4e75408aff32ac693e
Martin De Guzman <57105334+KuYuh02@users.noreply.github.com>
2025-03-05 18:49:16 -0800

    Update publickey.cpp

--- publickey.cpp
+++ publickey.cpp
@@ -1,114 +1,78 @@
-#include <iostream>
-#include <string>
-#include <unordered_set>
-#include <openssl/rsa.h>
-#include <openssl/pem.h>
-#include <openssl/err.h>
-#include <openssl/sha.h>
+#include <gmpxx.h>
+#include <stdexcept>
+#include <utility>
 
-// Part 1: Password Guessing
-class PasswordGuesser {
-private:
-    std::unordered_set<std::string> database; // Simulated database of passwords
-    std::unordered_set<std::string> classmates; // Simulated classmates' passwords
-    int guessCounter;
+// This is just a convenient name for the key pair
+typedef std::pair<mpz_class, mpz_class> keypair;
 
-public:
-    PasswordGuesser() : guessCounter(0) {
-        // Initialize with some example passwords (simulated database)
-        database.insert("pass1");
-        database.insert("123456");
-        database.insert("qwerty");
-        database.insert("letmein");
-        database.insert("admin12");
+// Test harness provides the hash function that we will use for
+// signature verification. We are using tiny SHA values here
+to keep our messages small so that we can use smaller primes
+// in our key generation sequence.
+uint16_t sha16(const mpz_class& key);
 
-        // Simulated classmates' passwords
-        classmates.insert("class1");
-        classmates.insert("mate22");
-        classmates.insert("friend");
-    }
-
-    int guess(const std::string& password) {
-        guessCounter++;
-        if (database.find(password) != database.end()) {
-            if (classmates.find(password) != classmates.end()) {
-                return 10; // Classmate's password
-            }
-            return 1; // Regular password in the database
+// It is often better if we are going to do base ** exponent mod modulus
+// to do it in a single operation to keep the numbers from getting too big
+// Not just true here, but we can use the same idea for normal integers to
+// help keep values in range. There are a lot of articles on how to do this
+// Find one and translate it to work with Gnu Multi-precision numbers
+// Hint: To check if the exponent is odd, use
+//   mpz_odd_p(exponent.get_mpz_t())) {
+mpz_class power_mod(mpz_class base, mpz_class exponent, const mpz_class& modulus) {
+    mpz_class result(1);
+    base = base % modulus;
+    while (exponent > 0) {
+        if (mpz_odd_p(exponent.get_mpz_t())) {
+            result = result * base % modulus;
         }
-        return 0; // Password not found
-    }
-};
-
-// Part 2: Public Key Cryptography
-class PublicKeyCrypto {
-private:
-    RSA* privateKey; // Your private key
-    RSA* publicKey;  // Your public key
-    RSA* theirPublicKey; // Their public key
-
-public:
-    PublicKeyCrypto() : privateKey(nullptr), publicKey(nullptr), theirPublicKey(nullptr) {
-        // Generate your own RSA key pair
-        generateKeyPair();
+        exponent = exponent / 2;
+        base = base * base % modulus;
     }
 
-    ~PublicKeyCrypto() {
-        if (privateKey) RSA_free(privateKey);
-        if (publicKey) RSA_free(publicKey);
-        if (theirPublicKey) RSA_free(theirPublicKey);
+    return result;
+}
+
+// GnuMP has already written this for you... it's in the original C interface
+// though... Wrapped it to make it easier to call in C++
+mpz_class modular_multiplicative_inverse(const mpz_class& a, const mpz_class& b) {
+    mpz_class result;
+    if (not mpz_invert(result.get_mpz_t(), a.get_mpz_t(), b.get_mpz_t())) {
+        throw std::runtime_error("domain error");
+    }
+    return result;
+}
+
+// Save your private key and the last public key you sent me
+mpz_class private_key;
+keypair public_key;
+
+keypair create_keys(const mpz_class& p, const mpz_class& q) {
+    mpz_class n = p * q;
+    mpz_class phi = (p - 1) * (q - 1);
+    mpz_class e(65537);
+    mpz_class d = modular_multiplicative_inverse(e, phi);
+
+    public_key = {e, n};
+    private_key = d;
+
+    return public_key;
+}
+
+// You will write a message validator. I will send you a message encoded with
+// your public key... I computed the SHA16 hash of the message and encoded it
+// using my PRIVATE key. So, you can recover the hash using my PUBLIC key.
+// If the hash doesn't match hash(decoded cypher), it is a forgery!! Encode
+// the message 'f' to let me know you intercepted a bogus message. Otherwise,
+// if I did sign this message, send back 'e' to let me know that it was an even
+// number or 'o' to let me know it was an odd. Encode it using my public key
+// so I can decode it securely.
+mpz_class validate(const mpz_class& cypher, const mpz_class& signature, const keypair& pats_public_key) {
+    mpz_class decoded_message = power_mod(cypher, private_key, public_key.second);
+    mpz_class decoded_hash = power_mod(signature, pats_public_key.first, pats_public_key.second);
+
+    if (sha16(decoded_message) != decoded_hash.get_ui()) {
+        return power_mod(mpz_class('f'), pats_public_key.first, pats_public_key.second);
     }
 
-    void generateKeyPair() {
-        privateKey = RSA_new();
-        BIGNUM* e = BN_new();
-        BN_set_word(e, RSA_F4);
-        RSA_generate_key_ex(privateKey, 2048, e, nullptr);
-        BN_free(e);
-
-        // Extract public key from private key
-        publicKey = RSAPublicKey_dup(privateKey);
-    }
-
-    void setTheirPublicKey(RSA* key) {
-        theirPublicKey = RSAPublicKey_dup(key);
-    }
-
-    std::string decryptMessage(const std::string& encrypted) {
-        std::string decrypted(RSA_size(privateKey), '\0');
-        int len = RSA_private_decrypt(encrypted.size(), 
-                                     reinterpret_cast<const unsigned char*>(encrypted.data()),
-                                     reinterpret_cast<unsigned char*>(&decrypted[0]),
-                                     privateKey, RSA_PKCS1_OAEP_PADDING);
-        if (len == -1) {
-            throw std::runtime_error("Decryption failed");
-        }
-        decrypted.resize(len);
-        return decrypted;
-    }
-
-    std::string encryptMessage(const std::string& message) {
-        std::string encrypted(RSA_size(theirPublicKey), '\0');
-        int len = RSA_public_encrypt(message.size(),
-                                    reinterpret_cast<const unsigned char*>(message.data()),
-                                    reinterpret_cast<unsigned char*>(&encrypted[0]),
-                                    theirPublicKey, RSA_PKCS1_OAEP_PADDING);
-        if (len == -1) {
-            throw std::runtime_error("Encryption failed");
-        }
-        encrypted.resize(len);
-        return encrypted;
-    }
-
-    bool verifyHash(const std::string& message, const std::string& hash) {
-        // Simulate hash verification using their public key
-        unsigned char digest[SHA256_DIGEST_LENGTH];
-        SHA256_CTX sha256;
-        SHA256_Init(&sha256);
-        SHA256_Update(&sha256, message.data(), message.size());
-        SHA256_Final(digest, &sha256);
-
-        std::string computedHash(reinterpret_cast<char*>(digest), SHA256_DIGEST_LENGTH);
-        return computedHash == hash;
-    }
-};
+    return power_mod(mpz_class(mpz_even_p(decoded_message.get_mpz_t()) ? 'e' : 'o'), pats_public_key.first, pats_public_key.second);
+}
